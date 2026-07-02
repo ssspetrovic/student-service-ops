a548ab38ca3bca981a761a20467bcc2e05be0578
Srdjan Petrovic <s.petrovic852@gmail.com>
2026-07-03 01:11:56 +0200

    test: add curriculum course basic tests

--- backend/academics/tests.py
+++ backend/academics/tests.py
@@ -2,7 +2,8 @@ from django.db import IntegrityError
 from django.test import TestCase
 
 from accounts.models import User, UserRole, ProfessorProfile
-from .models import Course
+from .models import Course, Curriculum, CurriculumCourse, DegreeLevel
+
 
 # Create your tests here.
 class CourseEspbTestCase(TestCase):
@@ -30,4 +31,62 @@ class CourseEspbTestCase(TestCase):
                 name="Test Course",
                 espb=61,
                 professor=self.professor,
+            )
+
+
+class CurriculumCourseUniqueTestCase(TestCase):
+    def setUp(self):
+        self.professor_user = User.objects.create_user(
+            email="professor@example.com",
+            password="professor123",
+            role=UserRole.PROFESSOR,
+        )
+        self.professor = ProfessorProfile.objects.create(user=self.professor_user)
+
+        self.course = Course.objects.create(
+            code="TCOURSE",
+            name="Test Course",
+            espb=60,
+            professor=self.professor,
+        )
+
+        self.curriculum = Curriculum.objects.create(
+            code="TCURR",
+            name="Test Curriculum",
+            degree_level=DegreeLevel.MASTER,
+            duration=3,
+        )
+
+    def test_curriculum_course_uniqueness(self):
+        CurriculumCourse.objects.create(
+            curriculum=self.curriculum,
+            course=self.course,
+            semester=1,
+            school_year="2020/2021",
+        )
+
+        with self.assertRaises(IntegrityError):
+            CurriculumCourse.objects.create(
+                curriculum=self.curriculum,
+                course=self.course,
+                semester=3,
+                school_year="2020/2021",
+            )
+
+    def test_curriculum_course_reject_semester_below_min(self):
+        with self.assertRaises(IntegrityError):
+            CurriculumCourse.objects.create(
+                curriculum=self.curriculum,
+                course=self.course,
+                semester=0,
+                school_year="2020/2021",
+            )
+
+    def test_curriculum_course_reject_semester_above_max(self):
+        with self.assertRaises(IntegrityError):
+            CurriculumCourse.objects.create(
+                curriculum=self.curriculum,
+                course=self.course,
+                semester=5,
+                school_year="2020/2021",
             )
