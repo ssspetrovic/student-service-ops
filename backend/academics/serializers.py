f5002b35d17059e8b54f70d2fb954a2c7e940c74
Srdjan Petrovic <s.petrovic852@gmail.com>
2026-07-12 16:27:44 +0200

    chore: add academics serializers

---
+++ backend/academics/serializers.py
@@ -0,0 +1,60 @@
+from rest_framework import serializers
+
+from .models import Course, Curriculum, CurriculumCourse, Enrollment
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    professor_email = serializers.CharField(source="professor.user.email")
+    professor_employee_no = serializers.CharField(
+        source="professor.employee_no",
+        read_only=True,
+    )
+
+    class Meta:
+        model = Course
+        fields = ["code", "name", "espb", "professor_email", "professor_employee_no"]
+
+
+class CurriculumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curriculum
+        fields = ["code", "name", "degree_level", "duration"]
+
+
+class CurriculumCourseSerializer(serializers.ModelSerializer):
+    curriculum_code = serializers.CharField(source="curriculum.code")
+    curriculum_name = serializers.CharField(source="curriculum.name")
+    course_code = serializers.CharField(source="course.code")
+    course_name = serializers.CharField(source="course.name")
+
+    class Meta:
+        model = CurriculumCourse
+        fields = [
+            "curriculum_code",
+            "curriculum_name",
+            "course_code",
+            "course_name",
+            "semester",
+            "is_mandatory",
+            "school_year",
+        ]
+
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    course_code = serializers.CharField(source="course.code")
+    course_name = serializers.CharField(source="course.name")
+    course_espb = serializers.IntegerField(source="course.espb")
+    student_index_no = serializers.CharField(source="student.index_no")
+
+    class Meta:
+        model = Enrollment
+        fields = [
+            "course_code",
+            "course_name",
+            "course_espb",
+            "student_index_no",
+            "school_year",
+            "semester",
+            "status",
+            "enrolled_at",
+        ]
